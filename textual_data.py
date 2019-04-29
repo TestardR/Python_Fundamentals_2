7c943e1f0f7c42c7971e5ac95d9db9688e8bc9d1
unknown <rmtestard@gmail.com>
2019-04-29 18:12:54 +0200

    few methods

--- textual_data.py
+++ textual_data.py
@@ -1,3 +1,20 @@
 message = 'Hello World'
 
 print(message)
+print(len(message))
+print(message[10])
+print(message[:5])
+print(message[6:])
+print(message.upper())
+print(message.count('o'))
+print(message.find('World'))
+
+new_message = message.replace('World', 'Romain')
+
+print(new_message)
+
+greeting = 'Hello'
+name = 'Vanessa'
+
+last_message = f'Hello {name}. Welcome!'
+print(last_message)
