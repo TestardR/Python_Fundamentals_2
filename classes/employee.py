5b43e1d2d1b107c22072437e8966ab767ff95328
unknown <rmtestard@gmail.com>
2019-05-02 11:19:30 +0200

    class methods and static methods

--- classes/employee.py
+++ classes/employee.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Employee:
 
     num_of_emps = 0
@@ -16,6 +19,21 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
 print(Employee.num_of_emps)
@@ -37,3 +55,13 @@ print(emp_1.__dict__)
 print(Employee.__dict__)
 
 print(Employee.num_of_emps)
+
+Employee.set_raise_amt(1.05)
+print(emp_1.raise_amount)
+
+emp_str_3 = 'John-Doe-70000'
+new_emp_3 = Employee.from_string(emp_str_3)
+print(new_emp_3.email)
+
+my_date = datetime.date(2016, 7, 10)
+print(Employee.is_workday(my_date))
