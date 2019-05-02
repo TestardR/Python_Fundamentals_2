b0618b4569d61c053610398ee1e503d5f11606f1
unknown <rmtestard@gmail.com>
2019-05-02 11:02:26 +0200

    classes variable

--- classes/employee.py
+++ classes/employee.py
@@ -1,14 +1,24 @@
 class Employee:
 
+    num_of_emps = 0
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = f'{first}.{last}@company.com'
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return f'{self.first} {self.last}'
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+print(Employee.num_of_emps)
 
 emp_1 = Employee('Romain', 'Testard', 50000)
 emp_2 = Employee('Vanessa', 'Renaud', 80000)
@@ -21,3 +31,9 @@ print(emp_2.email)
 
 print(emp_1.fullname())
 print(emp_2.fullname())
+
+print(emp_1.raise_amount)
+print(emp_1.__dict__)
+print(Employee.__dict__)
+
+print(Employee.num_of_emps)
